261eb9bb44bdfe9dbcdbeea9911023bac31656b1
aravindh2106 <aravindhan@logicsoft.co.in>
2022-02-16 11:25:15 +0530

    Removed unnecessary permission from Manifest file

--- app/src/main/java/in/co/logicsoft/getimagefromstorage/MainActivity.kt
+++ app/src/main/java/in/co/logicsoft/getimagefromstorage/MainActivity.kt
@@ -1,15 +1,11 @@
 package `in`.co.logicsoft.getimagefromstorage
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.FileNotFoundException
 
@@ -19,37 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pick_btn.setOnClickListener {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    Array(1) { Manifest.permission.READ_EXTERNAL_STORAGE },
-                    IMAGE_REQUEST_CODE
-                )
-            } else {
-                pickedImage()
-            }
-        }
-    }
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == IMAGE_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             pickedImage()
+        }
     }
 
     private fun pickedImage() {
         val galleryIntent = Intent(
-            Intent.ACTION_PICK,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        )
-        galleryIntent.type = "image/*"
+            Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
         startActivityForResult(galleryIntent, IMAGE_REQUEST_CODE)
     }
 
@@ -58,10 +33,12 @@ class MainActivity : AppCompatActivity() {
         val contentResolver = applicationContext.contentResolver
         if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
             try {
-                val imageUri = data!!.data
-                val inputStream = contentResolver.openInputStream(imageUri!!)
+                val imageUri = data?.data
+                val takeFlags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                contentResolver.takePersistableUriPermission(imageUri!!, takeFlags)
+                val inputStream = contentResolver.openInputStream(imageUri)
                 val selectedImage = BitmapFactory.decodeStream(inputStream)
-                Log.i("selectedImage", selectedImage.toString())
+                Log.i("selectedImage", imageUri.toString())
                 imageView.setImageBitmap(selectedImage)
             } catch (e: FileNotFoundException) {
                 e.printStackTrace()
